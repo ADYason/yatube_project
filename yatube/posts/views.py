df12ae2d66a93a92ccd17b0a2b73de9c8b6fa030
Goryainov Yaroslav <goryainov.yaroslav@gmail.com>
2021-09-26 18:34:42 +0300

    almoust done

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from typing import Text
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post, Group
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'text':posts,
+    }
+    return render(request, template, context)
 
 def group_list(request):
-    return HttpResponse('Список групп')
+    template = 'posts/group_list.html'
+    context = {
+        'text':'Здесь будет информация о группах проекта Yatube',
+    }
+    return render(request, template, context)
 
 def group_posts(request, slug):
-    return HttpResponse('Пост номер {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    template = 'posts/group_posts.html'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
